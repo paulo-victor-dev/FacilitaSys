4b4af0069e1088010f979865a82fe02ac74c6498
paulo-victor-dev <vikktor.paulo@gmail.com>
2025-06-11 17:12:30 -0300

    adjust templates layout and dashboard

--- static/js/main.js
+++ static/js/main.js
@@ -1,8 +1,13 @@
 const body = document.querySelector("body"),
       sidebar = body.querySelector(".sidebar"),
-      btn_toggle = body.querySelector(".btn_toggle");
+      btn_toggle = body.querySelector(".btn_toggle"),
+      user_area = body.querySelector(".user_area");
 
 btn_toggle.addEventListener("click", () => {
     sidebar.classList.toggle("close");
     
+});
+
+user_area.addEventListener("click", () => {
+    user_area.classList.toggle("active")
 });
