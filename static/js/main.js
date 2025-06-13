cfb74dc31451b4432830701c108491d80d38f47c
paulo-victor-dev <vikktor.paulo@gmail.com>
2025-06-13 15:59:29 -0300

    uploading all CSS em HTML modifications

--- static/js/main.js
+++ static/js/main.js
@@ -1,7 +1,8 @@
 const body = document.querySelector("body"),
       sidebar = body.querySelector(".sidebar"),
       btn_toggle = body.querySelector(".btn_toggle"),
-      user_area = body.querySelector(".user_area");
+      user_area = body.querySelector(".user_area"),
+      dropdown_menu = body.querySelector(".dropdown_menu")
 
 btn_toggle.addEventListener("click", () => {
     sidebar.classList.toggle("close");
@@ -10,4 +11,15 @@ btn_toggle.addEventListener("click", () => {
 
 user_area.addEventListener("click", () => {
     user_area.classList.toggle("active")
+});
+
+
+document.addEventListener("click", function(event){
+    if (
+        user_area.classList.contains("active") &&
+        !user_area.contains(event.target) &&
+        !dropdown_menu.contains(event.target)
+    ) {
+        user_area.classList.remove("active")
+    }
 });
